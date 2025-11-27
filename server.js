a7002ce1a7dd7cb88e07cd1df59e38512a3c5e1b
TheLAZARUS17 <167510479+TheLAZARUS17@users.noreply.github.com>
2025-11-27 14:04:36 +0000

    Add package.json

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("<b> Christian Jay C. Godinez </b> <br>");
-  res.write("BSIT BA 4102 <br>");
-  res.write("<i> The lion does not concern himself with the opinions of sheep. </i> <br>");
+  res.write(`
+    <div style="font-family: Helvetica;">
+      <div style="font-size: 28px; font-weight: bold;">
+        Christian Jay C. Godinez
+      </div>
+      <div style="font-size: 24px;">
+        BSIT BA 4102
+      </div>
+      <div style="font-size: 32px; font-style: italic; text-align: center; margin-top: 10px;">
+        The lion does not concern himself with the opinions of sheep.
+      </div>
+    </div>
+  `);
   res.end();
 });
 
