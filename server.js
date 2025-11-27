9649f2afdefd0c193e34313bf5b01f96a5900210
TheLAZARUS17 <167510479+TheLAZARUS17@users.noreply.github.com>
2025-11-27 16:47:23 +0000

    Add packages.json

--- server.js
+++ server.js
@@ -4,53 +4,59 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.write(`
     <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Simple HTTP Server</title>
-      <style>
+<html>
+
+<head>
+    <title>Simple HTTP Server</title>
+    <style>
         body {
-          margin: 0;
-          height: 100vh;
-          display: flex;
-          justify-content: center;
-          background-color: #e1e0ddff;
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #fff;
         }
 
         .card {
-          background-color: #fff;
-          padding: 40px 60px;
-          border-radius: 8px;
-          box-shadow: 0 10px 30px rgba(0, 0, 0, 0.6);
+            background-color: #ff3700ff;
+            color: #fff;
+            padding: 20px 20px;
+            border-radius: 12px;
+            box-shadow: 0 10px 30px rgba(0, 0, 0, 0.6);
         }
 
         .info {
-          font-family: Inter Tight, serif;
-          font-size: 18px;
-          text-align: left;
+            font-family: Inter Tight, serif;
+            font-size: 12px;
+            text-align: left;
+            color: #ff3700ff;
         }
 
         .quote {
-          font-family: Inter Tight, serif;
-          font-size: 36px;
-          font-style: italic;
-          text-align: center;
-          margin-top: 20px;
+            font-family: Inter Tight, serif;
+            font-size: 45px;
+            text-align: center;
+            margin-top: 40px;
         }
-      </style>
-    </head>
-    <body>
-      <div class="card">
-        <div class="info">
-          <h1>CHRISTIAN JAY C. GODINEZ</h1>
-          <h2>BSIT BA 4102</h2>
-        </div>
+    </style>
+</head>
 
-        <div class="quote">
-          <p>"The <strong>lion</strong> does not concern himself with the opinions of <strong>sheep</strong>."</p>
+<body>
+    <div>
+        <div class="info">
+            <h1>CHRISTIAN JAY C. GODINEZ</h1>
+            <h2>BSIT BA 4102</h2>
+            <p>Just a simple student with outrageous dreams.</p>
         </div>
-      </div>
-    </body>
-    </html>
+        <div class="card">
+            <div class="quote">
+                <p>"The <em>lion</em> does not concern himself with the opinions of <em>sheep</em>."</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
   `);
   res.end();
 });
